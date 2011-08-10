90e53a75d18eeb0d308aca239a129bef9ea9a29e
Malaney Hill <malaney.hill@nytimes.com>
2011-08-10 11:40:07 -0400

    Now, the install script will not only symlink .vimrc, but also pull down all submodules

--- install-vimrc.sh
+++ install-vimrc.sh
@@ -3,7 +3,15 @@ cd ..
 ln -s .vim/vimrc .vimrc
 ln -s .vim/gvimrc .gvimrc
 
+# Tell git not to verify SSL for submodules
+export GIT_SSL_NO_VERIFY=true
+
+# Install all submodules
+git submodule update --init
+
 # xmledit
-cd .vim/bundle/xmledit/ftplugin/
-ln -s xml.vim html.vim
-ln -s xml.vim xhtml.vim
+# cd .vim/bundle/xmledit/ftplugin/
+# ln -s xml.vim html.vim
+# ln -s xml.vim xhtml.vim
+
+
